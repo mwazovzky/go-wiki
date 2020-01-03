e8c8c821376386e12b4f460069f5b6ab40cf6fc3
Mike Wazovzky <mike.wazovzky@gmail.com>
2020-01-03 13:24:05 +0300

    Write/read page to/from files

--- wiki.go
+++ wiki.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := "pages/" + p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := "pages/" + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
 
 func main()  {
-	fmt.Println("Hello World!")
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a test page body")}
+	p1.save()
+	p2, err := loadPage("TestPage")
+	if err != nil {
+		fmt.Println("Error")
+		return
+	}
+	fmt.Println(string(p2.Body))
 }
