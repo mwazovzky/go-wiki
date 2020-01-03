8399f690a5d4f3ea2f2108caeb22aadecbf8defc
Mike Wazovzky <mike.wazovzky@gmail.com>
2020-01-03 17:54:00 +0300

    Error handling

--- wiki.go
+++ wiki.go
@@ -48,13 +48,25 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/save/"):]
     body := r.FormValue("body")
     p := &Page{Title: title, Body: []byte(body)}
-    p.save()
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
     http.Redirect(w, r, "/view/" + title, http.StatusFound)
 }
 
 func renderTemplate(w http.ResponseWriter, templ string, p *Page)  {
-	t, _ := template.ParseFiles("templates/" + templ + ".html")
-	t.Execute(w, p)
+	t, err := template.ParseFiles("templates/" + templ + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main()  {
